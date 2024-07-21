ff633b5fb150ffdce1df0acd2767562560ec9e85
gabr-el <gabrielbacate@outlook.com>
2024-07-20 21:30:05 -0300

    Removendo arquivo temporário 'teste.sql'

--- teste.sql
+++
@@ -1,9 +0,0 @@
-create table usuarios(
-    nome varchar(50),
-    email varchar(100),
-    idade int
-);
-
-insert into usuarios(nome, email, idade) values(
-    "Lucas Maik", "email.teste.com", 15
-);
